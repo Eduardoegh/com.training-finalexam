b770d5312aeba705fd8b03b8df38c57e44560b80
brandon.gonzalez <eduardoegh95@gmail.com>
2019-05-31 15:56:52 -0500

    Primera subida

---
+++ examan-training-2/src/main/java/com/training/finalexam/examantraining2/ExamanTraining2Application.java
@@ -0,0 +1,13 @@
+package com.training.finalexam.examantraining2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamanTraining2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamanTraining2Application.class, args);
+	}
+
+}
